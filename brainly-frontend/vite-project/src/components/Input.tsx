47e609a5e3b827eded4a4a6e0eedbf544088e653
ishu25122004 <kanishklodha25@gmail.com>
2026-08-30 22:53:22 +0530

    chore: remove vercel config and update Input component

--- brainly-frontend/vite-project/src/components/Input.tsx
+++ brainly-frontend/vite-project/src/components/Input.tsx
@@ -1,6 +1,6 @@
  export function Input({ placeholder,ref, type = "text", label}:{placeholder:string; ref:React.Ref<HTMLInputElement>; type?: string; label?: string }){
     return<div className="mb-3">
-        {label && <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1.5 pl-1">{label}</label>}
-        <input ref={ref} placeholder={placeholder} type={type} className="px-4 py-2 border border-gray-300 dark:border-gray-700 rounded-lg w-full text-black dark:text-white bg-white dark:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-indigo-500/50 transition-all placeholder:text-gray-400 dark:placeholder:text-gray-500"></input>
+        {label && <label className="block text-sm font-medium text-black dark:text-gray-300 mb-1.5 pl-1">{label}</label>}
+        <input ref={ref} placeholder={placeholder} type={type} className="px-4 py-2 border border-gray-300 dark:border-gray-700 rounded-lg w-full text-black dark:text-white bg-white dark:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-indigo-500/50 transition-all placeholder:text-black dark:placeholder:text-white"></input>
     </div>
 }
